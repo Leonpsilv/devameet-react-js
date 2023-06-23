2841b737f483f8a493bf288ffc26a91bf826dff4
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-06-23 15:12:38 -0400

    login page and api integration

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,11 @@
-import React from 'react'
-import ReactDOM from 'react-dom/client'
-import { Login } from './views/login.tsx'
+import React from "react";
+import ReactDOM from "react-dom/client";
+import { Login } from "./views/Login";
 
-ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
-  <React.StrictMode>
-    <Login />
-  </React.StrictMode>,
-)
+import "./assets/styles/global.scss";
+
+ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
+    <React.StrictMode>
+        <Login />
+    </React.StrictMode>
+);
