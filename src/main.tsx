5942f9dcfbbc6dce1cc6c6be62f0f6794f5a1f51
Leonpsilv <leonardo.pin.s2002@gmail.com>
2023-07-19 17:43:42 -0400

    update

--- src/main.tsx
+++ src/main.tsx
@@ -1,12 +1,10 @@
-import React from "react";
-import ReactDOM from "react-dom/client";
-import { RouterProvider } from "react-router-dom";
-import { router } from "./router";
+import React from 'react'
+import ReactDOM from 'react-dom/client'
+import './assets/styles/global.scss';
+import { App } from './App';
 
-import "./assets/styles/global.scss";
-
-ReactDOM.createRoot(document.getElementById("root") as HTMLElement).render(
-    <React.StrictMode>
-        <RouterProvider router={router} />
-    </React.StrictMode>
-);
+ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>,
+)
--- src/services/RegisterServices.ts
+++ src/services/RegisterServices.ts
@@ -1,7 +1,7 @@
 import { HttpApiServices } from "./HttpApiServices";
 
 export class RegisterServices extends HttpApiServices {
-    register(body: any) {
-        return this.post("/auth/register", body);
+    register(body:any){
+        return this.post('/auth/register', body);
     }
 }
